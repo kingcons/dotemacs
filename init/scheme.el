5e90d3378e455c524eab4f07e07b75f6ea15e2f9
Brit Butler <redline6561@gmail.com>
2011-01-12 19:07:15 -0500

    Adjustments to enable M-- M-x slime with chicken.

--- init/scheme.el
+++ init/scheme.el
@@ -1,5 +1,12 @@
+;; Geiser for Guile
 (add-to-list 'load-path "~/emacs/site-lisp/geiser/elisp")
-
 (setq geiser-impl-installed-implementations '(guile))
-
 (require 'geiser)
+
+;; Swank and SLIME for Chicken
+(add-to-list 'load-path "~/emacs/site-lisp/swank-chicken")
+(autoload 'chicken-slime "chicken-slime" "SWANK backend for Chicken" t)
+(setq swank-chicken-path "~/emacs/site-lisp/swank-chicken/swank-chicken.scm")
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (slime-mode t)))
