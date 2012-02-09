67b9033c46c637d84c6503656bd8c512e56bd142
Brit Butler <redline6561@gmail.com>
2012-02-09 15:14:46 -0500

    Minor multi-term improvements.

--- init/terminal.el
+++ init/terminal.el
@@ -2,3 +2,9 @@
 (autoload 'multi-term-next "multi-term" nil t)
 
 (setq multi-term-program "/bin/bash")
+
+(add-to-list 'term-bind-key-alist '("C-y" . term-paste))
+(add-to-list 'term-bind-key-alist '("C-<backspace>" . term-send-backward-kill-word))
+(add-to-list 'term-bind-key-alist '("M-<backspace>" . term-send-backward-kill-word))
+
+(global-set-key (kbd "C-c t") 'multi-term)
