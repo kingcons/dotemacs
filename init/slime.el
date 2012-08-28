78581e7027284605d2aa77ccaf0df1265ccd8db8
Brit Butler <redline6561@gmail.com>
2012-08-28 16:27:36 -0400

    Trim a bunch of old, unneeded stuff from slime.el.

--- init/slime.el
+++ init/slime.el
@@ -1,6 +1,3 @@
-;;;; In *most* regrettable fashion, there is still a great divide
-;;;; between Common Lisp swank/slime and every other implementation.
-;;;; Will these atrocities ever end?
 (add-to-list 'load-path "~/emacs/site-lisp/slime")
 
 ; Quicklisp is awesome...but let's manage this ourselves...
@@ -8,7 +5,6 @@
 
 ;; Set implementations and character encoding
 ; C-u M-x slime will prompt you for what lisp implementation to use.
-; M-- M-x slime works too. What's the difference?
 (setq slime-lisp-implementations
   '((ccl  ("ccl64" "-K utf-8"))
     (ecl  ("ecl"))
@@ -61,30 +57,6 @@
 
 ;; I've got the HyperSpec locally, why aren't I using it?
 (setq common-lisp-hyperspec-root "/home/redline/builds/HyperSpec/")
-
-;; Old SLIME code removed by Helmut on 2008-03-14. I use it to run
-;; multiple inferior-lisps for different langs (i.e. clj, scm, cl) concurrently.
-(defun slime-find-connection-by-name (name)
-  (find name slime-net-processes
-        :test #'string= :key #'slime-connection-name))
-
-(defun slime-select-connection-by-ext (extension-name)
-  `((string= type ,(first extension-name))
-    (setq slime-buffer-connection
-          (slime-find-connection-by-name ,(second extension-name)))))
-
-(setq slime-connections-map '(("lisp" "sbcl")
-                              ("asd" "sbcl")
-                              ("clj" "clojure")))
-
-(defmacro assign-correct-connection ()
-  `(let ((type ,(file-name-extension (buffer-name))))
-     (cond ,@(mapcar #'slime-select-connection-by-ext slime-connections-map))))
-
-(add-hook 'slime-mode-hook (lambda () (assign-correct-connection)))
-
-; Everybody loves Parenscript...
-;(load "js-expander.el")
 
 ; Misc...
 ;(setq slime-enable-evaluate-in-emacs t)
