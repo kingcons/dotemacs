0d0009ea4eebea057859e3258906cddf09f9535b
Brit Butler <redline6561@gmail.com>
2012-08-29 17:12:32 -0400

    Begone foul demons!

--- init/clojure.el
+++ init/clojure.el
@@ -1,9 +1,5 @@
 (require 'clojure-mode)
 
-(add-hook 'clojure-mode-hook
-          '(lambda ()
-             (define-key clojure-mode-map
-               "\C-c\C-e" 'lisp-eval-last-sexp)
-             (define-key clojure-mode-map
-               "\C-x\C-e" 'lisp-eval-last-sexp)))
+(remove-hook 'clojure-mode-hook 'clojure-enable-slime)
+(add-hook 'clojure-mode-hook 'nrepl-eldoc-enable-in-current-buffer)
 
