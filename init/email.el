d79193233986c4cf7c4c7373b7eae19b1ed24aa4
Brit Butler <brit@kingcons.io>
2022-01-26 13:14:37 -0500

    Update minimal config for mu4e.

--- init/email.el
+++ init/email.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t; -*-
+
 ;;; Get pass configured.
 
 (use-package pass
@@ -11,6 +13,8 @@
       mm-default-directory "~/Downloads"
       message-kill-buffer-on-exit t
       message-send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-service 465
+      smtpmail-stream-type 'ssl
       smtpmail-servers-requiring-authorization "smtp.migadu.com")
 
 ;;;  Get ERC configured while we're doing e-things.
@@ -26,29 +30,26 @@
 ;; Configure mu4e
 
 (defun bsb/make-context (name address folder)
-  (let ((smtp-gmail '((smtpmail-smtp-server . "smtp.gmail.com")
-                      (smtpmail-stmp-service . 465)
-                      (smtpmail-stream-type . ssl)))
-        (smtp-migadu '((smtpmail-smtp-server . "smtp.migadu.com")
-                       (smtpmail-smtp-service . 587)
-                       (smtpmail-stream-type . starttls))))
+  (let ((smtp-gmail '(smtpmail-smtp-server . "smtp.gmail.com"))
+        (smtp-migadu '(smtpmail-smtp-server . "smtp.migadu.com")))
     (make-mu4e-context :name name
                        :match-func (lambda (msg)
                                      (when msg
                                        (mu4e-message-contact-field-matches msg :to address)))
-                       :vars `(,@(if (string-match-p "kingcons.io" address) smtp-migadu smtp-gmail)
+                       :vars `(,(if (string-match-p "kingcons.io" address) smtp-migadu smtp-gmail)
                                (user-mail-address . ,address)
                                (mu4e-sent-folder . ,(concat folder "/Sent Mail"))
-                               (mu4e-trash-folder . ,(concat folder "/Trash")))))
+                               (mu4e-trash-folder . ,(concat folder "/Trash"))))))
 
 (unless (eq system-type 'darwin)
   (use-package mu4e
     :bind (("s-e" . mu4e))
     :config
     (setq mu4e-context-policy 'pick-first
+          mu4e-compose-context-policy 'ask-if-none
           mu4e-get-mail-command "mbsync -a"
           mu4e-change-filenames-when-moving t)
     (setq mu4e-contexts `(,(bsb/make-context "calendly" "b.butler@calendly.com" "/calendly[Gmail]")
                           ,(bsb/make-context "britton" "britton.s.butler@gmail.com" "/britton[Gmail]")
                           ,(bsb/make-context "redline" "redline6561@gmail.com" "/redline[Gmail]")
-                          ,(bsb/make-context "kingcons" "brit@kingcons.io" "/kingcons"))))))
+                          ,(bsb/make-context "kingcons" "brit@kingcons.io" "/kingcons")))))
