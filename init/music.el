0d67ba7970b477acc892c1056e68e77877e81077
Brit Butler <brit@kingcons.io>
2024-08-20 07:19:00 -0400

    Make it easier to load playlists, fix emms window restore.
    There's finicky interaction between the existing show and display hooks
    for emms and my window restoring stuff depending on run order.
    
    Advising the functions feels perhaps a bit excessive but it does work.
    And I don't want to spend more time on this problem so 👍

--- init/music.el
+++ init/music.el
@@ -1,16 +1,19 @@
 (use-package emms
   :pin gnu
+  :bind
+  (:map emms-playlist-mode-map
+        ("o" . bsb/emms-load-playlist))
+  (:map emms-browser-mode-map
+        ("o" . bsb/emms-load-playlist))
   :config
   (emms-minimalistic)
   (require 'emms-cache)
   (require 'emms-browser)
-  (require 'emms-history)
   (require 'emms-mode-line)
   (require 'emms-tag-editor)
   (require 'emms-info-native)
   (require 'emms-last-played)
   (require 'emms-playlist-mode)
-  (emms-history-load)
   ; To eval on first run only. Expect some lag.
   ; Takes about ~11m for 3600 flacs: 300 tracks/min.
   ; Worth running an explicit (emms-cache-save) after just to be sure.
@@ -18,11 +21,10 @@
   ; TODO: Boy would it be nice if EMMS synced when new dirs are added.
   (emms-cache 1)
   (emms-mode-line-enable)
-  (add-hook 'emms-browser-show-display-hook 'bsb/store-windows)
-  (add-hook 'emms-browser-hide-display-hook 'bsb/restore-windows)
+  (advice-add 'emms-browser-display-playlist :before 'bsb/store-windows)
+  (advice-add 'emms-browser-hide-linked-window :after 'bsb/restore-windows)
   ; NOTE: If this binding doesn't work, frob Gnome/Mutter with:
   ; gsettings set org.gnome.mutter.keybindings switch-monitor "[]"
-  (global-set-key (kbd "s-p") 'emms-smart-browse)
   (setq emms-player-list '(emms-player-vlc)
         emms-info-functions '(emms-info-native)
         emms-mode-line-mode-line-function #'bsb/emms-mode-line-blurb
@@ -45,6 +47,12 @@
          (title (bsb/truncate-string (emms-track-get track 'info-title) desc-limit)))
     (format bsb/emms-mode-line-format artist title)))
 
+(defun bsb/emms-load-playlist (file)
+  (interactive
+   (let ((playlist-dir (expand-file-name "playlists/" bsb/config-dir)))
+     (list (read-file-name "Pkaylist file: " playlist-dir))))
+  (emms-source-add 'emms-source-playlist file))
+
 (defun bsb/random-album ()
   (interactive)
   (emms-browse-by-album)
@@ -52,3 +60,4 @@
     (goto-line (random count))))
 
 (global-set-key (kbd "s-r") 'bsb/random-album)
+(global-set-key (kbd "s-p") 'emms-smart-browse)
